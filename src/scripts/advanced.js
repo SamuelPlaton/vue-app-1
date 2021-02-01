0db8438a93de166053894f000cf733eda50e2cc3
= <=>
2021-02-01 13:10:02 +0100

    feat: advanced function

--- src/scripts/advanced.js
+++ src/scripts/advanced.js
@@ -2,25 +2,52 @@ new Vue({
   el: '#app',
   data: {
     books: [],
-    searchString: "",
+    booksFound: [],
     checkedCategories: [],
     checkedLanguages: [],
     matureContent: false,
+    bookLength: "",
+    showAll: false,
   },
-  computed: {
-    filteredBooks: function () {
-      // language
-      // category
-      // averageRating
-      // maturity rating
-      // pageCount
-    }
-  },
+    methods:{
+      findBooks: function(){
+        // Filter by category
+        let filteredCategories= this.books.filter(book => book.volumeInfo?.categories?.filter(category => this.checkedCategories.includes(category)).length > 0);
+        // Filter by language
+        let filteredLanguages = filteredCategories.filter(book => this.checkedLanguages.includes(book.volumeInfo.language));
+        // Filter if mature content
+        if(!this.matureContent){
+          filteredLanguages = filteredLanguages.filter(book => book.volumeInfo.maturityRating === 'NOT_MATURE');
+        }
+        // Filter by length
+        let filteredLength = [];
+        if(this.bookLength === 'short'){
+          filteredLength = filteredLanguages.filter(book => book.volumeInfo.pageCount <= 300)
+        }else if (this.bookLength === 'medium'){
+          filteredLength = filteredLanguages.filter(book => book.volumeInfo.pageCount > 300 && book.volumeInfo.pageCount <= 600)
+        }else if (this.bookLength === 'long'){
+          filteredLength = filteredLanguages.filter(book => book.volumeInfo.pageCount > 600)
+        }
+        // Filter by rating
+        const filteredRank = filteredLength.sort(function(a, b){
+          const ratingA = a.volumeInfo?.averageRating ?? 0;
+          const ratingB = b.volumeInfo?.averageRating ?? 0;
+          if ( ratingA > ratingB ){
+            return -1;
+          }
+          if ( ratingA < ratingB ){
+            return 1;
+          }
+          return 0;
+        });
+        this.booksFound = filteredRank;
+        console.log(filteredRank);
+      }
+    },
   beforeCreate() {
     axios
-      .get('https://www.googleapis.com/books/v1/volumes?q=search-terms&key=AIzaSyDb5RvxGkCOKziqW5juAkV4IT3aW25dN_Y')
+      .get('https://www.googleapis.com/books/v1/volumes?q=search-terms&key=AIzaSyDb5RvxGkCOKziqW5juAkV4IT3aW25dN_Y&maxResults=40')
       .then(res => {
-        console.log(res.data.items[0]);
         this.books = res.data.items
       })
   }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -19,14 +19,14 @@ new Vue({
       if(this.categorySelected.length === 0){
         categoryFiltered = titleFiltered;
       }else{
-        categoryFiltered = titleFiltered.filter(book => book.volumeInfo.categories.includes(this.categorySelected))
+        categoryFiltered = titleFiltered.filter(book => book.volumeInfo?.categories?.includes(this.categorySelected))
       }
       return categoryFiltered;
     }
   },
   beforeCreate() {
     axios
-      .get('https://www.googleapis.com/books/v1/volumes?q=search-terms&key=AIzaSyDb5RvxGkCOKziqW5juAkV4IT3aW25dN_Y')
+      .get('https://www.googleapis.com/books/v1/volumes?q=search-terms&key=AIzaSyDb5RvxGkCOKziqW5juAkV4IT3aW25dN_Y&maxResults=40')
       .then(res => {
         console.log(res.data.items[0]);
         this.books = res.data.items
